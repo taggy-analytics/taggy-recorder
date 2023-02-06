b56c3d155eb5f40b41ede358691fe1f449bf5f75
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2023-02-06 16:09:11 +0100

    Create additions_0002.php

---
+++ database/additions_0002.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('cache', function ($table) {
+            $table->string('key')->unique();
+            $table->text('value');
+            $table->integer('expiration');
+        });
+    }
+};
