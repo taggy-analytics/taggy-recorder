fb993f4682315bc42ef15997a42929c023eb9fec
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2023-02-23 19:12:42 +0100

    Update RtspCamera.php

--- app/CameraTypes/RtspCamera.php
+++ app/CameraTypes/RtspCamera.php
@@ -22,7 +22,7 @@ abstract class RtspCamera extends CameraType
         $outputFile = $outputDirectory . '/video.m3u8';
         File::makeDirectory($outputDirectory, recursive: true);
         // $this->runFFmpegCommand($this->getRtspUrl($camera), $outputFile, '-codec copy -start_number 0 -hls_time ' . config('taggy-recorder.video-conversion.segment-duration') . ' -hls_list_size 0 -f hls');
-        $this->runFFmpegCommand($this->getRtspUrl($camera), $outputFile, '-c:v libx264 -c:a copy -preset medium -crf 22 -start_number 0 -hls_time ' . config('taggy-recorder.video-conversion.segment-duration') . ' -hls_list_size 0 -f hls');
+        $this->runFFmpegCommand($this->getRtspUrl($camera), $outputFile, '-codec copy');
     }
 
     public function stopRecording(Camera $camera)
