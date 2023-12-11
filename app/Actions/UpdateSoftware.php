c5aa8b39ba0fe9827603435bc1afad720318b451
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2023-12-11 18:40:48 +0100

    Update UpdateSoftware.php

--- app/Actions/UpdateSoftware.php
+++ app/Actions/UpdateSoftware.php
@@ -61,9 +61,16 @@ class UpdateSoftware
             Process::run('composer install');
             Process::run('php artisan migrate --force');
 
+            /*
             // As long as we are not API exclusively
             Process::run('npm install');
             Process::run('npm run build');
+            */
+
+            // Do final checks before activating new release
+            if(!File::exists($releasePath . '/vendor')) {
+                throw new \Exception('Vendor directory does not exist.');
+            }
 
             unlink($releasePath . '/../../current');
             symlink($releasePath, $releasePath . '/../../current');
