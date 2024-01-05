180f624f84ef81aa106861c61b880733335a3268
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2024-01-06 00:22:27 +0100

    Update additions_0027.php

--- database/migrations/additions_0027.php
+++ database/migrations/additions_0027.php
@@ -10,11 +10,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('cameras', function (Blueprint $table) {
-            $table->float('rotation', 12, 9)->default(Camera::DEFAULT_ROTATION)->after('started_at');
+            $table->float('rotation', 12, 9)->default(Camera::DEFAULT_ROTATION)->after('recording_mode');
         });
 
         Schema::table('recordings', function (Blueprint $table) {
-            $table->float('rotation', 12, 9)->nullable()->after('recording_mode');
+            $table->float('rotation', 12, 9)->nullable()->after('started_at');
         });
     }
 };
