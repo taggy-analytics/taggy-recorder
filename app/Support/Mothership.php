5ebe1682f12a1b235e3aef843f2d2ee7d5b0d89e
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2024-01-06 13:52:13 +0100

    Update Mothership.php

--- app/Support/Mothership.php
+++ app/Support/Mothership.php
@@ -149,7 +149,7 @@ class Mothership
 
     public function sendTemperatureLog($data)
     {
-        return $this->post('recorders/' . Recorder::make()->getSystemId() . '/temperature?key=' . config('taggy-recorder.mothership-logging-key'), $data);
+        return $this->post('recorders/' . Recorder::make()->getSystemId() . '/temperature?key=' . config('taggy-recorder.mothership-logging-key'), ['measurements' => $data]);
     }
 
     private function get($url, $type = 'json')
