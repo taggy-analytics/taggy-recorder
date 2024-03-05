b6631d2b1d636aadd8841de2aa5c138a82c97840
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2024-03-05 13:44:33 +0100

    Update ReolinkDuo3Poe.php

--- app/CameraTypes/ReolinkDuo3Poe.php
+++ app/CameraTypes/ReolinkDuo3Poe.php
@@ -8,5 +8,5 @@ class ReolinkDuo3Poe extends Reolink
     public CONST VIDEO_HEIGHT = 2170;
 
     // used to identify the camera type from the API DevInfo response
-    protected const MODEL_NAME = 'Reolink Duo 2 PoE';
+    protected const MODEL_NAME = 'Reolink Duo 3 PoE';
 }
