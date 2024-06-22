11854d70deaf1689fbcc2d076e62ae5df4277cf8
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2024-06-22 11:55:18 +0200

    x

---
+++ stall_debug_logger.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the desired directory
+cd /var/www/taggy/current
+
+# Create a virtual environment
+python3 -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Install psutil package
+pip install psutil
+
+# Run the stall_debug_logger.py script in the background, redirecting output to /dev/null
+python3 ./stall_debug_logger.py & >/dev/null
