e6e13edd1a667843a6a3d2de64fbaafb115b53a3
Sebastian Schöps <sebastian.schoeps@nanuc.io>
2025-02-26 03:21:46 +0100

    Update ReolinkDuo3Poe.php

--- app/CameraTypes/ReolinkDuo3Poe.php
+++ app/CameraTypes/ReolinkDuo3Poe.php
@@ -7,7 +7,7 @@ use App\Enums\VideoFormat;
 class ReolinkDuo3Poe extends Reolink
 {
     public CONST VIDEO_WIDTH = 7680;
-    public CONST VIDEO_HEIGHT = 2170;
+    public CONST VIDEO_HEIGHT = 2160;
 
     // used to identify the camera type from the API DevInfo response
     protected const MODEL_NAME = 'Reolink Duo 3 PoE';
